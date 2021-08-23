8666c9e3d88af9188b80eb453c9cdb2d49000eee
Rafael Ponte <rponte@gmail.com>
2021-08-22 21:12:24 -0300

    job: performance: initialDelay + Hibernate batch size;

--- src/main/java/br/com/zup/edu/nobank/cards/AttachCardsToProposalsJob.java
+++ src/main/java/br/com/zup/edu/nobank/cards/AttachCardsToProposalsJob.java
@@ -36,17 +36,25 @@ public class AttachCardsToProposalsJob {
      *
      * 1. Naive solution
      * 2. Top50 (aka pagination) only = finishes too early
-     * 2.1. Top50 + loop = long-running transaction
+     *  2.1. Top50 + loop = long-running transaction
      * 3. Top50 sem @Transactional = inconsistência nos dados
      * 4. Top50 + transactionManager
      * 5. cluster? Top50 + transactionManager + synchronized = fail
      * 6. cluster? Top50 + transactionManager + FOR UPDATE = work but 1 node only
      * 7. cluster? Top50 + transactionManager + FOR UPDATE SKIP LOCKED = WOW!
-     * 8. performance? Hibernate batch size, initialDelay=xxx
-     * 9. error handling? timeout, retries, idempotence
-     * 10. What if...
+     * 8. performance?
+     *  8.0. initialDelay=xxx
+     *  8.1. Hibernate batch size
+     *  8.2. database specifics: https://vladmihalcea.com/postgresql-multi-row-insert-rewritebatchedinserts-property/
+     *  8.3. database tuning with DBA help
+     *  8.4. another ORM like jOOQ or MyBatis
+     *  8.5. stored procedures
+     * 9. error handling?
+     *  9.1. timeout, retries, idempotence
+     *  9.2. bulkhead pattern: avoid shared connection pool
+     * 11. What if...
      */
-    @Scheduled(fixedDelay = ONE_MINUTE)
+    @Scheduled(initialDelay = ONE_MINUTE, fixedDelay = ONE_MINUTE)
     public void execute() {
 
         logger.info("Verifying eligible proposals that have no attached cards yet...");
