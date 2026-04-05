bb0fff1e16928f9920b4cea19d18fa88ffc78fae
Rafael Ponte <rponte@gmail.com>
2026-04-05 20:02:43 -0300

    disables Scheduling on tests;

--- src/main/java/br/com/deveficiente/nobank/NobankApplication.java
+++ src/main/java/br/com/deveficiente/nobank/NobankApplication.java
@@ -2,12 +2,13 @@ package br.com.deveficiente.nobank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableRetry
-@EnableScheduling
 @EnableFeignClients // does NOT work on @Configuration classes: https://github.com/spring-cloud/spring-cloud-openfeign/issues/301
 @SpringBootApplication
 public class NobankApplication {
@@ -15,5 +16,10 @@ public class NobankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NobankApplication.class, args);
 	}
+
+	@Configuration
+	@EnableScheduling
+	@ConditionalOnProperty(name = "app.scheduling.enabled", havingValue = "true", matchIfMissing = true)
+	static class SchedulingConfiguration {}
 
 }
--- src/test/java/br/com/deveficiente/nobank/NobankApplicationTests.java
+++ src/test/java/br/com/deveficiente/nobank/NobankApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.deveficiente.nobank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NobankApplicationTests {
 
 	@Test
